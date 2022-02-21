68fcf4b8f71c56ea7601fe52df4a3e846b2b7047
sj608 <steven_psj@hotmail.com>
2022-02-21 17:18:09 +0900

    initial init

---
+++ DAC/DAC.py
@@ -0,0 +1,33 @@
+import spidev
+from gpiozero import LED as pin
+from DAC_REG import EDGEPI_DAC_ADDRESS as address
+from DAC_REG import EDGEPI_DAC_COM as command
+
+import logging
+logging.basicConfig(level=logging.INFO)
+_logger=logging.getLogger(__name__)
+
+class EdgePi_DAC():
+    def __init__ (self):
+        self.spi = spidev.SpiDev()
+        self.spi.open(6, 0)
+        self.spi.no_cs = True
+        self.spi.max_speed_hz = 1000000
+        self.spi.mode = 2
+        # CS is connected on GPIO18
+        self.cs = pin(18)
+        self.cs.on()
+
+    def combine_command(self, op_code, ch, value):
+        temp = op_code<<20 + ch<<16 + value
+        list = [temp>>16, (temp>>8)&0xFF, temp&0xFF]
+        return list
+
+    def write_and_update(self, ch, data):
+        command = self.combine_command(command.COM_WRITE_UPDATE.value, address(ch).value)
+        self.cs.off()
+        spi.xfer(command)
+        self.cs.on()
+        
+        
+
---
+++ DAC/DAC_REG.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class EDGEPI_DAC_COM(Enum):
+    COM_NOP = 0x0
+    COM_WRITE_INPUT = 0x1 # LDAC pin is held low, so not used
+    COM_UPDATE_DAC = 0x2
+    COM_WRITE_UPDATE = 0x3
+    COM_POWER_DOWN_OP = 0x4
+    COM_HW_LDAC_MASK = 0x5 # LDAC pin is held low, so not used 
+    COM_SW_RESET = 0x6 # resets to POR, address bits set to 0x0 and data bits set to 0x1234
+    COM_GAIN = 0x7
+    COM_DCEN = 0x8 #Daisy chain setup enable
+    COM_READBACK = 0x9 # used for daisy chain operation
+    COM_UPDATE_ALL_CH = 0x10 # update all channels of the input register simultaneously with the input data
+    COM_UPDATE_ALL_DAC = 0x11 # update all channel of teh input register and DAC register simultaneously with the input data
+
+class EDGEPI_DAC_ADDRESS(Enum):
+    DAC0 = 0x0
+    DAC1 = 0x1
+    DAC2 = 0x2
+    DAC3 = 0x3
+    DAC4 = 0x4
+    DAC5 = 0x5
+    DAC6 = 0x6
+    DAC7 = 0x7
