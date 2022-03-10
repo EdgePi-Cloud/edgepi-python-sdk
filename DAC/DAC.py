5e9a4e0bb8f59359ced11c7222a2f8706239d696
sj608 <steven_psj@hotmail.com>
2022-03-10 18:42:52 +0900

    added conversion functions

--- DAC/DAC.py
+++ DAC/DAC.py
@@ -7,10 +7,12 @@ import logging
 logging.basicConfig(level=logging.INFO)
 _logger=logging.getLogger(__name__)
 
+VOLTAGE_REF = 2.047
+RANGE = 65536
+
 class EdgePi_DAC():
     def __init__ (self):
         self.spi = spidev.SpiDev()
-        self.spi.open(6, 0)
         self.spi.no_cs = True
         self.spi.max_speed_hz = 1000000
         self.spi.mode = 2
@@ -25,15 +27,25 @@ class EdgePi_DAC():
 
     def write_and_update(self, ch, data):
         command = self.combine_command(command.COM_WRITE_UPDATE.value, address(ch).value)
+        self.spi.open(6, 0)
         self.cs.off()
         self.spi.xfer(command)
         self.cs.on()
+        self.spi.close()
 
     def sw_reset(self):
         command = self.combine_command(command.COM_SW_RESET, 0, 4660)
+        self.spi.open(6, 0)
         self.cs.off()
         self.spi.xfer(command)
         self.cs.on()
+        self.spi.close()
 
-        
+    def write_voltage_channel(self, ch, voltage):
+        code = self.voltage_to_code(voltage)
+        self.write_and_update(ch, code)
 
+#ToDo: change the formula according to calibration if needed
+    def voltage_to_code(self, expected):
+        code = expected * (RANGE/VOLTAGE_REF)
+        return code
