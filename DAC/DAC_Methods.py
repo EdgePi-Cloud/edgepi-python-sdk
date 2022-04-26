55da373b411dd1c15fa252dde20e3ff35aec3f36
sjpark608 <spark@osensa.com>
2022-04-26 15:07:40 -0700

    Added value check functions
    Realized more test cases are required for combine_command function

--- DAC/DAC_Methods.py
+++ DAC/DAC_Methods.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from pickle import EMPTY_LIST
 from DAC_Constants import EDGEPI_DAC_ADDRESS as ADDRESS
 from DAC_Constants import EDGEPI_DAC_COM as COMMAND
 
@@ -11,7 +13,30 @@ class DAC_Methods():
         _logger.info(f'Initializing DAC Methods')
 
     def combine_command(self, op_code, ch, value):
-        temp = (op_code<<20) + (ch<<16) + value
-        list = [temp>>16, (temp>>8)&0xFF, temp&0xFF]
-        _logger.debug(f'Combined Command is: {list}')
+        if self.check_for_int([op_code, ch, value]):
+            temp = (op_code<<20) + (ch<<16) + value
+            list = [temp>>16, (temp>>8)&0xFF, temp&0xFF]
+            _logger.debug(f'Combined Command is: {list}')
+        else:
+            # Todo: throw an exception instead?
+            list = None
         return list
+
+    def write_and_update(self, ch, data):
+        return self.combine_command(COMMAND.COM_WRITE_UPDATE.value, ADDRESS(ch).value, data)
+    
+    #@staticmethod
+    def check_for_int(self, target_list):
+        if not target_list:
+            return False
+        for i in range(len(target_list)):
+            if not isinstance(target_list[i], int):
+                _logger.debug(f'Non-Integer number detected: {i}')
+                return False
+        return True
+
+    def check_range(self, target, min, max):
+        if target < max and target > min:
+            return True
+        else:
+            return False
