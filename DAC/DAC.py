80e3c15cc6b6508364446c55ad995b46bf8a3f8a
sjpark608 <spark@osensa.com>
2022-05-12 12:40:08 -0700

    Update DAC.py
    inherit form the spi module #7

--- DAC/DAC.py
+++ DAC/DAC.py
@@ -4,15 +4,15 @@ from Peripherals.Peripherals import SpiDevice as spi
 import logging
 _logger=logging.getLogger(__name__)
 
-class EdgePiDAC():
+class EdgePiDAC(spi):
     def __init__ (self):
         _logger.info(f'Initializing DAC Bus')
-        self.spi = spi(dev_path='/dev/spidev6.3', mode = 1, max_speed=1000000)
+        super().__init__(dev_path='/dev/spidev6.3', mode = 1, max_speed=1000000)
         self.dac_ops = DACCommands()
 
     def write_voltage_channel(self, ch, voltage):
         code = self.dac_ops.voltage_to_code(voltage)
-        self.spi.transfer(self.dac_ops.generate_write_and_update_command(ch, code))
+        self.transfer(self.dac_ops.generate_write_and_update_command(ch, code))
 
     def close_spi(self,):
-        self.spi.close()
+        self.close()
