479242b0f90a454eeaa39ac3f2af4396cf6796dd
sjpark608 <spark@osensa.com>
2022-05-12 12:30:50 -0700

    Adding peripheral middleware module
    the middleware is intended to limit the scope of HW component usage of users to prevent damaging the board.

--- DAC/DAC.py
+++ DAC/DAC.py
@@ -1,5 +1,5 @@
-from DAC.DACCommands import DACCommands
-from periphery import SPI
+from DAC.DAC_Commands import DACCommands
+from Peripherals.Peripherals import SpiDevice as spi
 
 import logging
 _logger=logging.getLogger(__name__)
@@ -7,18 +7,12 @@ _logger=logging.getLogger(__name__)
 class EdgePiDAC():
     def __init__ (self):
         _logger.info(f'Initializing DAC Bus')
-        # Todo: SPI needs to have separate class and inherited
-        self.spi = self.initialize_spi(SPI)
-        # CS is connected on GPIO18
+        self.spi = spi(dev_path='/dev/spidev6.3', mode = 1, max_speed=1000000)
         self.dac_ops = DACCommands()
-
-    def initialize_spi(self, spi):
-        # Bus 6 dev 2, mode 1, max_freq = 1MHz
-        return SPI("/dev/spidev6.2", 1, 1000000)
 
     def write_voltage_channel(self, ch, voltage):
         code = self.dac_ops.voltage_to_code(voltage)
-        self.spi.tranfer(self.dac_ops.generate_write_and_update_command(ch, code))
+        self.spi.transfer(self.dac_ops.generate_write_and_update_command(ch, code))
 
     def close_spi(self,):
         self.spi.close()
---
+++ Peripherals/Peripherals.py
@@ -0,0 +1,10 @@
+from periphery import*
+
+# TODO: add more edgepi specific functions as needed
+class SpiDevice(SPI):
+    def __init__ (self, dev_path:str = None, mode:int = 1, max_speed:int = 1000000, bit_order:str='msb', bits_per_word:int = 8, extra_flags:int = 0):
+        super().__init__(dev_path, mode, max_speed, bit_order, bits_per_word, extra_flags)
+
+
+
+#TODO: Add other peripherals, I2C, GPIO
