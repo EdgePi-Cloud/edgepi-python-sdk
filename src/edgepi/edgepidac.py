3dc8043278b952b8fabcdc8afb95b16022facb9e
sjpark608 <spark@osensa.com>
2022-05-16 10:33:14 -0700

    changes applied

--- src/edgepi/edgepidac.py
+++ src/edgepi/edgepidac.py
@@ -1,5 +1,5 @@
 from edgepi.dac.dac_commands import DACCommands
-from edgepi.peripherals.peripherals import SpiDevice as spi
+from edgepi.peripherals.spi import SpiDevice as spi
 
 import logging
 _logger=logging.getLogger(__name__)
@@ -7,7 +7,7 @@ _logger=logging.getLogger(__name__)
 class EdgePiDAC(spi):
     def __init__ (self):
         _logger.info(f'Initializing DAC Bus')
-        super().__init__(dev_path='/dev/spidev6.3', mode = 1, max_speed=1000000)
+        super().__init__(devID=3, mode = 1, max_speed=1000000)
         self.dac_ops = DACCommands()
 
     def write_voltage_channel(self, ch, voltage):
