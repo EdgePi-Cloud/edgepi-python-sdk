190e927c4cc732334bd9e4b71e41532d8a203dc1
sjpark608 <spark@osensa.com>
2022-05-15 22:04:05 -0700

    added protected class dictionary which maps device id to the device paths string added function so that it has transfer function. The peripheals module will be generic spi class. need to change the name

--- src/edgepi/EdgePiDAC.py
+++ src/edgepi/EdgePiDAC.py
@@ -7,7 +7,7 @@ _logger=logging.getLogger(__name__)
 class EdgePiDAC(spi):
     def __init__ (self):
         _logger.info(f'Initializing DAC Bus')
-        super().__init__(dev_path='/dev/spidev6.3', mode = 1, max_speed=1000000)
+        super().__init__(devID=3, mode = 1, max_speed=1000000)
         self.dac_ops = DACCommands()
 
     def write_voltage_channel(self, ch, voltage):
--- src/edgepi/peripherals/Peripherals.py
+++ src/edgepi/peripherals/Peripherals.py
@@ -1,10 +1,16 @@
-from periphery import*
-
-# TODO: add more edgepi specific functions as needed
-class SpiDevice(SPI):
-    def __init__ (self, dev_path:str = None, mode:int = 1, max_speed:int = 1000000, bit_order:str='msb', bits_per_word:int = 8, extra_flags:int = 0):
-        super().__init__(dev_path, mode, max_speed, bit_order, bits_per_word, extra_flags)
+from periphery import SPI
 
 
-
-#TODO: Add other peripherals, I2C, GPIO, etc
+# TODO: This class needs to be changed as the SPI library changes
+class SpiDevice():
+    _devIDtoPath ={
+            0:'/dev/spidev6.0',
+            1:'/dev/spidev6.1',
+            2:'/dev/spidev6.2',
+            3:'/dev/spidev6.3'
+            }
+    def __init__ (self, devID:int = None, mode:int = 1, max_speed:int = 1000000, bit_order:str='msb', bits_per_word:int = 8, extra_flags:int = 0):
+        self.spi = SPI(SpiDevice._devIDtoPath[devID], mode, max_speed, bit_order, bits_per_word, extra_flags)
+    
+    def transfer (self, data:list) -> list:
+        return self.spi.transfer(data)
