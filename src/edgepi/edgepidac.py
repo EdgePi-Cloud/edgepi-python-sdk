42c7d16acdd8a89e228aede776f7a3cb5015aa72
sjpark608 <spark@osensa.com>
2022-05-16 11:52:08 -0700

    requested changes applied
    - following semantic versioning
    - requirements.txt naming fix
    - SPI bus is fixed hardware wise, so doesn't make sense to pass as input argument
    - devID variable name change to dev_ID
    - removed close() function
    - removed .vscode folder

--- src/edgepi/edgepidac.py
+++ src/edgepi/edgepidac.py
@@ -8,13 +8,10 @@ _logger=logging.getLogger(__name__)
 class EdgePiDAC(spi):
     def __init__ (self):
         _logger.info(f'Initializing DAC Bus')
-        super().__init__(devID=3, mode = 1, max_speed=1000000)
+        super().__init__(dev_ID=3, mode = 1, max_speed=1000000)
 
         self.dac_ops = DACCommands(DACHwCalib_const, [DACSwCalib_const]*8)
 
     def write_voltage_channel(self, ch, voltage):
         code = self.dac_ops.voltage_to_code(voltage)
         self.transfer(self.dac_ops.generate_write_and_update_command(ch, code))
-
-    def close_spi(self,):
-        self.close()
--- src/edgepi/peripherals/spi.py
+++ src/edgepi/peripherals/spi.py
@@ -9,8 +9,8 @@ class SpiDevice():
             2:'/dev/spidev6.2',
             3:'/dev/spidev6.3'
             }
-    def __init__ (self, devID:int = None, mode:int = 1, max_speed:int = 1000000, bit_order:str='msb', bits_per_word:int = 8, extra_flags:int = 0):
-        self.spi = SPI(SpiDevice._devIDtoPath[devID], mode, max_speed, bit_order, bits_per_word, extra_flags)
+    def __init__ (self, dev_ID:int = None, mode:int = 1, max_speed:int = 1000000, bit_order:str='msb', bits_per_word:int = 8, extra_flags:int = 0):
+        self.spi = SPI(SpiDevice._devIDtoPath[dev_ID], mode, max_speed, bit_order, bits_per_word, extra_flags)
     
     def transfer (self, data:list) -> list:
         return self.spi.transfer(data)
