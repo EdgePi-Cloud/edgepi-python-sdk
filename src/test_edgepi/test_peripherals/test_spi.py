f5a1936fda742f08610a52f7b027ec7eb4042ecf
sjpark608 <spark@osensa.com>
2022-05-17 12:13:23 -0700

    Update test_spi.py

--- src/test_edgepi/test_peripherals/test_spi.py
+++ src/test_edgepi/test_peripherals/test_spi.py
@@ -1,10 +1,10 @@
 import pytest
 from edgepi.peripherals.spi import SpiDevice
 
-@pytest.mark.parametrize("dev_ID, bus_num, result",[(0, 6, '/dev/spidev0.6'), 
-                                                    (1, 6, '/dev/spidev1.6'), 
-                                                    (2, 6, '/dev/spidev2.6'), 
-                                                    (3, 6, '/dev/spidev3.6')])
+@pytest.mark.parametrize("dev_ID, bus_num, result",[(0, 6, '/dev/spidev6.0'), 
+                                                    (1, 6, '/dev/spidev6.1'), 
+                                                    (2, 6, '/dev/spidev6.2'), 
+                                                    (3, 6, '/dev/spidev6.3')])
 def test_check_range(dev_ID, bus_num, result):
     spi = SpiDevice(dev_ID, bus_num)
     assert spi.devPath == result
