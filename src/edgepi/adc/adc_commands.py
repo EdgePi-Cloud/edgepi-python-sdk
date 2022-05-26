ab6f7072720ec2939bd56a0c7f150f339ea7f47d
SJ Park <sj@SJs-MacBook-Air.local>
2022-05-26 15:36:23 -0700

    added write_register method, testing exception as well #17

--- src/edgepi/adc/adc_commands.py
+++ src/edgepi/adc/adc_commands.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from edgepi.adc.adc_constants import EDGEPI_ADC_CHANNEL as CH
 from edgepi.adc.adc_constants import EDGEPI_ADC_OP as opcode
 import logging
@@ -8,10 +9,18 @@ class ADCCommands():
         _logger.info(f'Initializing ADC Methods')
 
     def read_register_command(self, address, num):
-        ADCCommands.check_for_int([address, num])
+        self.check_for_int([address, num])
         command = [opcode.OP_RREG.value + address, num-1]
         _logger.debug(f'Command to send is {command + [255]*num}')
         return command + [255]*num
+
+    def write_register_command(self, address, values):
+        self.check_for_int([address])
+        all(self.check_range(value, 0, 255) for value in values)
+        self.check_for_int(values)
+        command = [opcode.OP_WREG.value + address, len(values)-1]
+        _logger.debug(f'Command to send is {command + values}')
+        return command + values
 
     @staticmethod
     def check_for_int(target_list):
@@ -19,3 +28,10 @@ class ADCCommands():
             return True
         else:
             raise ValueError(f'Non integer value passed{target_list}')
+
+    @staticmethod
+    def check_range(target, min, max):
+        if target <= max and target >= min:
+            return True
+        else:
+            raise ValueError(f'Target out of range {target}')
--- src/test_edgepi/test_adc/test_adc.py
+++ src/test_edgepi/test_adc/test_adc.py
@@ -16,6 +16,15 @@ def fixture_test_ADC_ops():
 def test_check_for_int(sample, result, adc_ops):
     assert adc_ops.check_for_int(sample) == result
 
+@pytest.mark.parametrize("sample, error", [([1,2.22,3,4, 0], ValueError), 
+                                            ([None, 1, 2, 3], ValueError), 
+                                            ([], ValueError), 
+                                            ([-1, -2.22], ValueError)])
+def test_check_for_int_exception(sample, error, adc_ops):
+    with pytest.raises(Exception) as e:
+        adc_ops.check_for_int(sample)
+    assert e.type is error
+
 """ 
 Test read register content
 In: Register address and number of register to read
@@ -25,6 +34,35 @@ Out: List of bytes Op-code and dummy bytes to transfer
                                                           (2, 4, [0x22, 3, 255, 255, 255, 255]), 
                                                           (3, 5, [0x23, 4, 255, 255, 255, 255, 255]), 
                                                           (10, 7, [0x2A, 6, 255, 255, 255, 255, 255, 255, 255]), 
-                                                          (16, 10, [0x30, 9, 255, 255, 255, 255, 255,255, 255, 255, 255, 255])])
+                                                          (16, 10, [0x30, 9, 255, 255, 255, 255, 255,255, 255, 255, 255, 255]),
+                                                          (16, 10, [0x30, 9, 255])])
 def test_read_register_command(address, num_of_regs, result, adc_ops):
     assert adc_ops.read_register_command(address, num_of_regs) == result
+
+@pytest.mark.parametrize("address, num_of_regs, error", [(1, 3.2, ValueError),
+                                                          (3.2, 1, ValueError)])
+def test_read_register_command_exception(address, num_of_regs, error, adc_ops):
+    with pytest.raises(Exception) as e:
+        adc_ops.read_register_command(address, num_of_regs)
+        assert e.type is error
+
+
+""" 
+Test write register content
+In: Register address and list of values to write
+Out: List of bytes Op-code and dummy bytes to transfer
+"""
+@pytest.mark.parametrize("address, values, result", [(1, [2], [0x41, 0, 2]),
+                                                     (2, [23, 25, 24, 102], [0x42, 3, 23, 25, 24, 102]), 
+                                                     (3, [243, 123, 125, 111, 231], [0x43, 4, 243, 123, 125, 111, 231]), 
+                                                     (10, [15, 25, 55, 225, 115, 2, 2], [0x4A, 6, 15, 25, 55, 225, 115, 2, 2]), 
+                                                     (16, [25, 55, 5, 2, 5, 25, 55, 55, 25, 2], [0x50, 9, 25, 55, 5, 2, 5, 25, 55, 55, 25, 2])])
+def test_write_register_command(address, values, result, adc_ops):
+    assert adc_ops.write_register_command(address, values) == result
+
+@pytest.mark.parametrize("address, values, error", [(1, [-1, 2], ValueError),
+                                                     (2, [23, 25, 256, 102], ValueError)])
+def test_write_register_command_exception(address, values, error, adc_ops):
+    with pytest.raises(Exception) as e:
+        adc_ops.write_register_command(address, values)
+        assert e.type is error
