78d5b33c1db58fb4eacb221e54016ad639e5aac1
flavius-t <flavius.t@hotmail.com>
2022-05-27 01:00:02 +0100

    tc module structure update

--- src/edgepi/tc/tc_commands.py
+++ src/edgepi/tc/tc_commands.py
@@ -2,3 +2,19 @@
 class TCCommands():
     def __init__(self, cs_line):
         self.cs = cs_line
+
+    def read_register(self):
+        ''' read the value of any MAX31856 register '''
+        pass
+
+    def write_to_register(self):
+        ''' write to any MAX31856 register '''
+        pass
+
+    def temp_to_code(self, temp:float):
+        ''' converts a float temperature value to binary code for writing to register '''
+        pass
+
+    def code_to_temp(self, code):
+        ''' converty register binary temperature code to float value'''
+        pass
