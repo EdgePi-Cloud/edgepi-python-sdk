1a5c1a076002d35e14417c82de0b4e91f3fd0bff
sjpark608 <spark@osensa.com>
2022-06-17 15:03:47 -0700

    Update gpio.py
    passing arguments without keyword

--- src/edgepi/peripherals/gpio.py
+++ src/edgepi/peripherals/gpio.py
@@ -8,7 +8,7 @@ class GpioDevice():
         self.pin_num = pin_num
         self.pin_dir = pin_dir
         self.pin_bias = pin_bias
-        self.gpio = GPIO(path = self.fd, line=self.pin_num, direction=self.pin_dir, edge='none', bias = self.pin_bias, drive="default" , inverted=False, label=None)
+        self.gpio = GPIO(self.fd, self.pin_num, self.pin_dir, 'none', self.pin_bias, "default" , False, None)
 
     def close(self):
         self.gpio.close()
