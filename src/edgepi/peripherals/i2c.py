1fd0536ed57a0dd724bcc1e38151d9834f48826a
sjpark608 <spark@osensa.com>
2022-06-25 05:00:54 +0100

    testing I2C

--- src/edgepi/peripherals/i2c.py
+++ src/edgepi/peripherals/i2c.py
@@ -6,5 +6,27 @@ class I2CDevice():
         self.fd = fd
         self.i2cdev = I2C(fd)
 
+    ''' 
+        set Read message to be sent through I2C.
+        Attributes:
+            addr: Register address to read from
+            Msg: list of place holder bytes
+        Return:
+            MsgList: list of I2C.Message() objects containing Msg to be sent
+    '''
+    def setReadMsg(self, addr:int = None, Msg:list = None):
+        return [self.i2cdev.Message([addr]), self.i2cdev.Message(Msg, read=True)]
+    
+    ''' 
+        set Write message to be sent through I2C.
+        Attributes:
+            addr: Register address to write to
+            Msg: list of Msg bytes
+        Return:
+            MsgList: list of I2C.Message() objects containing Msg to be sent
+    '''
+    def setWriteMsg(self, addr:int = None, Msg:list = None):
+        return [self.i2cdev.Message([addr]+Msg)]
+        
     def close(self):
         self.i2cdev.close()
--- src/test_edgepi/test_peripherals/test_i2c.py
+++ src/test_edgepi/test_peripherals/test_i2c.py
@@ -1,6 +1,7 @@
+from audioop import add
 import pytest
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import DEFAULT, patch
 import sys
 if sys.platform != 'linux':
     sys.modules['periphery'] = mock.MagicMock()
@@ -21,15 +22,46 @@ if sys.platform != 'linux':
 
 else:
     @pytest.mark.parametrize("fd, result",
-                            [( '/dev/i2c-10', ['/dev/i2c-10']),
-                             ( '/dev/i2c-11', ['/dev/i2c-11']),
-                             ( '/dev/i2c-12', ['/dev/i2c-12']),
-                             ( '/dev/i2c-13', ['/dev/i2c-13'])
+                            [( '/dev/i2c-10', ['/dev/i2c-10'])
                             ])
     def test_i2c_init_param(fd, result):
         i2cDevice = I2CDevice(fd)
         assert i2cDevice.fd == result[0]
-    # Set Read Message
+    
+
+    @pytest.mark.parametrize("addrs, Msg, result",
+                            [( 30, [31, 32], [[30], [31, 32], False, True]),
+                             ( 20, [21, 22], [[20], [21, 22], False, True]),
+                             ( 10, [11, 12], [[10], [11, 12], False, True]),
+                             ( 0, [1, 2], [[0], [1, 2], False, True])
+                            ])
+    @patch('edgepi.peripherals.i2c.I2C.Message')
+    def test_i2c_setReadMsg(i2c_mock, addrs, Msg, result):
+        i2c_mock().data1 = result[0]
+        i2c_mock().data2 = result[1]
+        i2c_mock().read1 = result[2]
+        i2c_mock().read2 = result[3]
+        i2cDevice = I2CDevice('/dev/i2c-10')
+        MsgList = i2cDevice.setReadMsg(addrs, Msg)
+        assert MsgList[0].data1 == result[0]
+        assert MsgList[1].data2 == result[1]
+        assert MsgList[0].read1 == result[2]
+        assert MsgList[1].read2 == result[3]
+
     # Get Read Message
     # Set Write Message
+    @pytest.mark.parametrize("addrs, Msg, result",
+                            [( 30, [31, 32], [[30, 31, 32], False]),
+                             ( 20, [21, 22], [[20, 21, 22], False]),
+                             ( 10, [11, 12], [[10, 11, 12], False]),
+                             ( 0, [1, 2], [[0, 1, 2], False])
+                            ])
+    @patch('edgepi.peripherals.i2c.I2C.Message')
+    def test_i2c_setWriteMsg(i2c_mock, addrs, Msg, result):
+        i2c_mock().data = result[0]
+        i2c_mock().read = result[1]
+        i2cDevice = I2CDevice('/dev/i2c-10')
+        MsgList = i2cDevice.setWriteMsg(addrs, Msg)
+        assert MsgList[0].data == result[0]
+        assert MsgList[0].read == result[1]
     # Get Write Message
