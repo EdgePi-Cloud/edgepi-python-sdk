b9244f6664e049a484f9fed1bac2eb8bfa07b834
flavius-t <flavius.t@hotmail.com>
2022-07-07 18:31:47 +0100

    lint fix gpio.py

--- src/edgepi/peripherals/gpio.py
+++ src/edgepi/peripherals/gpio.py
@@ -1,6 +1,15 @@
+"""
+Module for GPIO devices
+"""
+
+
 from periphery import GPIO
 
-class GpioDevice():
+
+# pylint: disable=too-few-public-methods
+class GpioDevice:
+    """Class for representing a GPIO device"""
+
     _dev_path = "/dev/gpiochip0"
 
     def __init__(self, pin_num: int = None, pin_dir: str = None, pin_bias: str = None):
@@ -11,4 +20,5 @@ class GpioDevice():
         self.gpio = GPIO(self.fd, self.pin_num, self.pin_dir, bias=self.pin_bias)
 
     def close(self):
+        """Close GPIO connection"""
         self.gpio.close()
