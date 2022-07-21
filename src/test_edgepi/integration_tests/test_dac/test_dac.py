640b4e14712ddf518db5616b39e4cded153e4a0b
flavius-t <flavius.t@hotmail.com>
2022-07-21 17:59:52 +0100

    expand integration tests

--- src/test_edgepi/integration_tests/test_dac/test_dac.py
+++ src/test_edgepi/integration_tests/test_dac/test_dac.py
@@ -13,6 +13,10 @@ import pytest
 from edgepi.dac.edgepi_dac import EdgePiDAC
 
 
+UPPER_VOLT_LIMIT = 5.117  # upper code limit with 3 dec place precision
+LOWER_VOLT_LIMIT = 0  # lower code limit
+
+
 @pytest.fixture(name="dac")
 def fixture_test_edgepi_dac():
     return EdgePiDAC()
@@ -21,12 +25,30 @@ def fixture_test_edgepi_dac():
 @pytest.mark.parametrize(
     "analog_out, voltage, raises",
     [
-        (8, 0, does_not_raise()),  # lower code limit
-        (8, 2.5, does_not_raise()),
-        (8, 5.117, does_not_raise()),  # upper code limit with 3 dec place precision
-        (1, 0, does_not_raise()),
+        (1, LOWER_VOLT_LIMIT, does_not_raise()),
         (1, 2.5, does_not_raise()),
-        (1, 5.117, does_not_raise()),
+        (1, UPPER_VOLT_LIMIT, does_not_raise()),
+        (2, LOWER_VOLT_LIMIT, does_not_raise()),
+        (2, 2.5, does_not_raise()),
+        (2, UPPER_VOLT_LIMIT, does_not_raise()),
+        (3, LOWER_VOLT_LIMIT, does_not_raise()),
+        (3, 2.5, does_not_raise()),
+        (3, UPPER_VOLT_LIMIT, does_not_raise()),
+        (4, LOWER_VOLT_LIMIT, does_not_raise()),
+        (4, 2.5, does_not_raise()),
+        (4, UPPER_VOLT_LIMIT, does_not_raise()),
+        (5, LOWER_VOLT_LIMIT, does_not_raise()),
+        (5, 2.5, does_not_raise()),
+        (5, UPPER_VOLT_LIMIT, does_not_raise()),
+        (6, LOWER_VOLT_LIMIT, does_not_raise()),
+        (6, 2.5, does_not_raise()),
+        (6, UPPER_VOLT_LIMIT, does_not_raise()),
+        (7, LOWER_VOLT_LIMIT, does_not_raise()),
+        (7, 2.5, does_not_raise()),
+        (7, UPPER_VOLT_LIMIT, does_not_raise()),
+        (8, LOWER_VOLT_LIMIT, does_not_raise()),
+        (8, 2.5, does_not_raise()),
+        (8, UPPER_VOLT_LIMIT, does_not_raise()),
         (1, -0.1, pytest.raises(ValueError)),
         (1, 5.118, pytest.raises(ValueError)),
         (0, 1.1, pytest.raises(ValueError)),
