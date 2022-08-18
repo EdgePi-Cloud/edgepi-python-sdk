e922c36fe1588c1b23a323167236fce6c0a65270
flavius-t <flavius.t@hotmail.com>
2022-08-18 21:04:03 +0100

    improve generate_mux_opcodes modularity

--- src/edgepi/adc/adc_multiplexers.py
+++ src/edgepi/adc/adc_multiplexers.py
@@ -12,6 +12,28 @@ class ChannelMappingError(ValueError):
     """Raised when an input channel is mapped to both ADC1 and ADC2"""
 
 
+def _format_mux_values(mux_p, mux_n):
+    # updating mux_p bits only, mask mux_p bits
+    if mux_n is None:
+        mask = BitMask.HIGH_NIBBLE
+        # replace None with 0 for building bitstring
+        mux_n_val = 0
+        mux_p_val = mux_p.value
+    # updating mux_n bits only, mask mux_n bits
+    elif mux_p is None:
+        mask = BitMask.LOW_NIBBLE
+        # replace None with 0 for building bitstring
+        mux_p_val = 0
+        mux_n_val = mux_n.value
+    # updating both mux_n and mux_p
+    else:
+        mask = BitMask.BYTE
+        mux_p_val = mux_p.value
+        mux_n_val = mux_n.value
+
+    return mux_p_val, mux_n_val, mask
+
+
 def generate_mux_opcodes(mux_updates: dict, mux_values: dict):
     """
     Generates list of OpCodes for updating mux mapping
@@ -34,39 +56,21 @@ def generate_mux_opcodes(mux_updates: dict, mux_values: dict):
         mux_p = byte[0]
         mux_n = byte[1]
 
-        mux_p_none = not bool(mux_p)
-        mux_n_none = not bool(mux_n)
-
         # not updating mux's for this adc_num (no args passed)
         if mux_p is None and mux_n is None:
             continue
-        # updating mux_p bits only, mask mux_p bits
-        elif mux_n is None:
-            mask = BitMask.HIGH_NIBBLE
-            # replace None with 0 for building bitstring
-            mux_n = 0
-            mux_p = mux_p.value
-        # updating mux_n bits only, mask mux_n bits
-        elif mux_p is None:
-            mask = BitMask.LOW_NIBBLE
-            # replace None with 0 for building bitstring
-            mux_p = 0
-            mux_n = mux_n.value
-        # updating both mux_n and mux_p
-        else:
-            mask = BitMask.BYTE
-            mux_p = mux_p.value
-            mux_n = mux_n.value
+
+        mux_p_val, mux_n_val, mask = _format_mux_values(mux_p, mux_n)
 
         # update mux_register values for duplicate mapping validation, but only
         # if these were updated
-        if not mux_p_none:
-            mux_values[addx][0] = mux_p
+        if not mux_p is None:
+            mux_values[addx][0] = mux_p_val
 
-        if not mux_n_none:
-            mux_values[addx][1] = mux_n
+        if not mux_n is None:
+            mux_values[addx][1] = mux_n_val
 
-        adc_x_ch_bits = pack("uint:4, uint:4", mux_p, mux_n).uint
+        adc_x_ch_bits = pack("uint:4, uint:4", mux_p_val, mux_n_val).uint
 
         mux_opcodes.append(OpCode(adc_x_ch_bits, addx.value, mask.value))
 
