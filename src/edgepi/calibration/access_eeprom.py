b9277cdd29cb60db5c3e579ee66cb0ad4e5e9528
sjpark608 <spark@osensa.com>
2022-09-22 17:01:52 -0700

    adding unittest

--- src/edgepi/calibration/access_eeprom.py
+++ src/edgepi/calibration/access_eeprom.py
@@ -1,1 +1,58 @@
 '''Helper class to access on board eeprom'''
+
+def sequential_read(bus, reg_addr: int = None, len: int = None, dev_addr: int = None):
+    '''
+    Read operation reads the specified number of memory location starting from provided address. The
+    address pointer will wrap around when it reaches the end of the memory.
+    Args:
+        bus: i2c bus
+        reg_addr: starting address to read from
+        len: length of data or number of data to read
+        dev_addr: device address
+    Returns:
+        List of read data
+
+    '''
+    msg = bus.set_read_msg(reg_addr, [0xFF]*len)
+    return bus.transfer(dev_addr, msg)
+
+def selective_read(bus, reg_addr: int = None, dev_addr: int = None):
+    '''
+    Read operation reads a data from the specified address
+    Args:
+        bus: i2c bus
+        reg_addr: starting address to read from
+        dev_addr: device address
+    Returns:
+        List of read data
+    '''
+    msg = bus.set_read_msg(reg_addr, [0xFF])
+    return bus.transfer(dev_addr, msg)
+
+def byte_write_register(bus, reg_addr: int = None, data: int = None, dev_addr: int = None):
+    '''
+    Write operation writes a data to the specified address
+    Args:
+        bus: i2c bus
+        reg_addr: starting address to read from
+        data: data to write to the location
+        dev_addr: device address
+    Returns:
+        N/A
+    '''
+    msg = bus.set_write_msg(reg_addr, [data])
+    bus.transfer(dev_addr, msg)
+
+def page_write_register(bus, reg_addr: int = None, data: int = None, dev_addr: int = None):
+    '''
+    Write operation writes a page of data to the specified address
+    Args:
+        bus: i2c bus
+        reg_addr: starting address to read from
+        data: data to write to the location
+        dev_addr: device address
+    Returns:
+        N/A
+    '''
+    msg = [bus.Message([reg_addr, data])]
+    bus.transfer(dev_addr, msg)
--- src/test_edgepi/unit_tests/test_calibration/test_access_eeprom.py
+++ src/test_edgepi/unit_tests/test_calibration/test_access_eeprom.py
@@ -1,1 +1,27 @@
 '''unit test for access eeprom'''
+
+from unittest import mock
+from unittest.mock import patch
+import sys
+sys.modules['periphery'] = mock.MagicMock()
+
+import pytest
+from edgepi.peripherals.i2c import I2CDevice
+from edgepi.calibration.access_eeprom import (
+    selective_read,
+    sequential_read,
+    byte_write_register,
+    page_write_register
+)
+
+@pytest.fixture(name="bus")
+def fixture_test_dac(mocker):
+    mocker.patch("edgepi.peripherals.i2c.I2C")
+    yield I2CDevice('/dev/i2c-10')
+
+@pytest.mark.parametrize("reg_addr, dev_addr, result", [(1, 32, 1)])
+def test_selective_read(mocker, bus, reg_addr, dev_addr, result):
+    mock_msg = mocker.patch("edgepi.peripherals.i2c.I2CDevice.Message", side_effect = [reg_addr, 255])
+    mock_msg.return_value = (mock_msg, mock_msg)
+    selective_read(bus, reg_addr, dev_addr)
+    check = result
