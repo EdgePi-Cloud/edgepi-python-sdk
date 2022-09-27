2fd28a3d1bd68469fab7113b569fed92df60d9ec
sjpark608 <spark@osensa.com>
2022-09-27 11:03:28 -0700

    Update test_access_eeprom.py

--- src/test_edgepi/unit_tests/test_calibration/test_access_eeprom.py
+++ src/test_edgepi/unit_tests/test_calibration/test_access_eeprom.py
@@ -21,7 +21,6 @@ def fixture_test_dac(mocker):
 
 @pytest.mark.parametrize("reg_addr, dev_addr, result", [(1, 32, 1)])
 def test_selective_read(mocker, bus, reg_addr, dev_addr, result):
-    mock_msg = mocker.patch("edgepi.peripherals.i2c.I2CDevice.Message", side_effect = [reg_addr, 255])
-    mock_msg.return_value = (mock_msg, mock_msg)
+    mock_set_msg = mocker.patch("edgepi.peripherals.i2c.I2CDevice.transfer", return_value = [])
     selective_read(bus, reg_addr, dev_addr)
     check = result
