9a14f416b46a7a831016e7e0c57ed49f929c2749
sjpark608 <spark@osensa.com>
2022-10-04 15:33:03 -0700

    adding functions to generate necessary dictionaries

--- src/edgepi/calibration/edgepi_calibration.py
+++ src/edgepi/calibration/edgepi_calibration.py
@@ -30,22 +30,28 @@ class EdgePiCalibration():
     def __init__(self, module: ModuleNames):
         self.module = module.value
         self.num_of_ch = NumOfCh[module.name].value
+        self.full_scale_range = None
+        self.full_scale_code = None
 
-    def __generate_calib_param_dict(self):
+    def generate_calib_param_dict(self):
         '''
         Function to generate dictionary of calibration parameters
         Args:
             N/A
         Return:
             ch_to_calib_dict (dict): dictionary mapped channel to CalibParam data class
-            ie) {1 : CalibParam(gain, offset)}
+            ie) {1 : CalibParam(gain, offset)
+                 .
+                 .
+                 .
+                 nth_channel : CalibParam(gain, offset)}
         '''
         ch_to_calib_dict = {}
         for ch in range(self.num_of_ch):
             ch_to_calib_dict[ch] = CalibParam(gain=1, offset=0)
         return ch_to_calib_dict
 
-    def __generate_measurements_dict(self, num_of_points):
+    def generate_measurements_dict(self, num_of_points: int = None):
         '''
         Function to generate dictionary of measurements to generate calibration parameter. It will
         store expected and actual measurements at each data point for number_of_points length
@@ -53,10 +59,43 @@ class EdgePiCalibration():
             num_of_points (int): number of points to be generated, used as length of the dictionary
         Return:
             measurements_dict (dict): dictionary mapping expected value to 
+            ie) {0 : {'input_unit' : int, 'expected_out' : float, 'actual_out' : float},
+                 .
+                 .
+                 .
+                 nth_point : {'input_unit' : int, 'expected_out' : float, 'actual_out' : float}}
         '''
         measurements_dict = {}
         for point in range(num_of_points):
-            measurements_dict[point]= {'expected': 0, 'actual': 0}
+            measurements_dict[point]= {'input_unit': 0 ,'expected_out': 0, 'actual_out': 0}
         return measurements_dict
 
-#TODO:  Least Square method
+    def generate_channel_measurements_dict(self, num_of_points: int = None):
+        '''
+        Function to generate measuremenst dictionary for each channel. This is neccessary to calibr-
+        -ate each channel individually
+        Args:
+            num_of_points (int): number of points to be generated, used as length of the dictionary
+        '''
+        ch_measurements_dict = {}
+        for ch in range(self.num_of_ch):
+            ch_measurements_dict[ch] = self.generate_measurements_dict(num_of_points)
+        return ch_measurements_dict
+
+    def record_measurements(self, nth_measurements: dict = None,
+                                  input_unit: int = None,
+                                  expected: float = None,
+                                  actual: float = None):
+        '''
+        Modify the expected and actual measurements of nth item of measuremnts dictionary 
+        Arg:
+            nth_measurements (dict): nth point in measurements dictionary
+            ie) {'expected' : float, 'actual' : float}
+            expected (float): expeceted measurements
+            actual (float): actual measurements
+        Return: 
+            N/A
+        '''
+        nth_measurements['input_unit'] = input_unit
+        nth_measurements['expected_out'] = expected
+        nth_measurements['actual_out'] = actual
