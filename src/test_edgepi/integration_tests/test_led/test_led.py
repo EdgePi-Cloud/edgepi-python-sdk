bde0f95539ded9c794f3015eaa88432e2f2b7f8f
flavius-t <77416463+flavius-t@users.noreply.github.com>
2022-10-19 15:46:01 -0700

    LED Module (#106)

---
+++ src/test_edgepi/integration_tests/test_led/test_led.py
@@ -0,0 +1,86 @@
+"""Integration tests for LED module"""
+
+import pytest
+
+from edgepi.gpio.gpio_configs import LEDPins, GpioConfigs
+from edgepi.gpio.edgepi_gpio import EdgePiGPIO
+from edgepi.led.edgepi_leds import EdgePiLED
+
+@pytest.fixture(name='led')
+def fixture_led():
+    return EdgePiLED()
+
+@pytest.fixture(name='gpio')
+def fixture_gpio():
+    return EdgePiGPIO(GpioConfigs.LED.value)
+
+@pytest.mark.parametrize('led_name', [
+    (LEDPins.LED_OVR1),
+    (LEDPins.LED_OVR2),
+    (LEDPins.LED_OVR3),
+    (LEDPins.LED_OVR4),
+    (LEDPins.LED_OVR5),
+    (LEDPins.LED_OVR6),
+    (LEDPins.LED_OVR7),
+    (LEDPins.LED_OVR8),
+])
+def test_turn_led_on(led_name, led, gpio):
+    led.turn_led_on(led_name)
+    state = gpio.read_expander_pin_state(led_name.value)
+    assert state is True
+
+
+@pytest.mark.parametrize('led_name', [
+    (LEDPins.LED_OVR1),
+    (LEDPins.LED_OVR2),
+    (LEDPins.LED_OVR3),
+    (LEDPins.LED_OVR4),
+    (LEDPins.LED_OVR5),
+    (LEDPins.LED_OVR6),
+    (LEDPins.LED_OVR7),
+    (LEDPins.LED_OVR8),
+])
+def test_turn_led_off(led_name, led, gpio):
+    led.turn_led_off(led_name)
+    state = gpio.read_expander_pin_state(led_name.value)
+    assert state is False
+
+
+@pytest.mark.parametrize('led_name', [
+    (LEDPins.LED_OVR1),
+    (LEDPins.LED_OVR2),
+    (LEDPins.LED_OVR3),
+    (LEDPins.LED_OVR4),
+    (LEDPins.LED_OVR5),
+    (LEDPins.LED_OVR6),
+    (LEDPins.LED_OVR7),
+    (LEDPins.LED_OVR8),
+])
+def test_get_led_state(led_name, led):
+    led.turn_led_on(led_name)
+    state = led.get_led_state(led_name)
+    assert state is True
+    led.turn_led_off(led_name)
+    state = led.get_led_state(led_name)
+    assert state is False
+
+
+@pytest.mark.parametrize('led_name', [
+    (LEDPins.LED_OVR1),
+    (LEDPins.LED_OVR2),
+    (LEDPins.LED_OVR3),
+    (LEDPins.LED_OVR4),
+    (LEDPins.LED_OVR5),
+    (LEDPins.LED_OVR6),
+    (LEDPins.LED_OVR7),
+    (LEDPins.LED_OVR8),
+])
+def test_toggle_led(led_name, led):
+    led.turn_led_on(led_name)
+    led.toggle_led(led_name)
+    state = led.get_led_state(led_name)
+    assert state is False
+    led.toggle_led(led_name)
+    state = led.get_led_state(led_name)
+    assert state is True
+    led.turn_led_off(led_name)
