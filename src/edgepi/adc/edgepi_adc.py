9cb29f548660cb9c77359beab6f0e1d5f35d227b
sjpark608 <spark@osensa.com>
2022-10-28 15:23:15 -0700

    Update edgepi_adc.py
    have a ruler set for pylint, and still fails on line-too-long :(

--- src/edgepi/adc/edgepi_adc.py
+++ src/edgepi/adc/edgepi_adc.py
@@ -98,7 +98,8 @@ class EdgePiADC(SPI):
         self.gpio = EdgePiGPIO(GpioConfigs.ADC.value)
         # internal state
         self.__state = ADCState(reg_map=None)
-        # TODO: remove this from init(), rename to reset_config, after init call reset onfig to reset registers.
+        # TODO: remove this from init(), rename to reset_config, after init call reset onfig to
+        # reset registers.
         self.__set_power_on_configs()
         # TODO: adc reference should ba a config that customer passes depending on the range of
         # voltage they are measuring. To be changed later when range config is implemented
@@ -113,7 +114,7 @@ class EdgePiADC(SPI):
             adc_1_mux_n=CH.AINCOM,
             checksum_mode=CheckMode.CHECK_BYTE_CRC,
             # TODO: have a manual function to clear the reset bit.
-            reset_clear=ADCPower.RESET_CLEAR, 
+            reset_clear=ADCPower.RESET_CLEAR,
         )
 
     def __read_register(self, start_addx: ADCReg, num_regs: int = 1):
