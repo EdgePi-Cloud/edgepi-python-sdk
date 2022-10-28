ec507bf1d544dcd68ced027a7ca6819fd2ccb4d9
sjpark608 <78987042+sjpark608@users.noreply.github.com>
2022-10-28 11:57:00 -0700

    Issue103 adc (#110)
    * adc testing being added
    
    * added test
    
    * Individual channel modification (#108)
    
    * Individual channel modification
    
    #103
    - Only single pins gets accessed
    - simplified code calculate for now, calibration to be added
    
    * fixed voltage_to_code and code_to_voltage calculation
    
    * applying suggested fix
    
    * added todo and fixed pylint
    
    * used enums for pin names
    
    * Update edgepi_adc.py
    
    removed clear exapnder to avoid resetting of Ground reference when a new object is instantiated

--- src/edgepi/adc/adc_constants.py
+++ src/edgepi/adc/adc_constants.py
@@ -190,6 +190,12 @@ class FilterMode(Enum):
     SINC4 = OpCode(0x60, ADCReg.REG_MODE1.value, ADCMasks.FILTER_BITS.value)
     FIR = OpCode(0x80, ADCReg.REG_MODE1.value, ADCMasks.FILTER_BITS.value)
 
+class ADCReferenceSwitching(Enum):
+    """ Each ADC Reference ground can be configured """
+    GND_SW_NONE = 0
+    GND_SW1 = 1
+    GND_SW2 = 2
+    GND_SW_BOTH = 3
 
 class ADCPower(Enum):
     """OpCodes for configuring the ADS1263 POWER register"""
