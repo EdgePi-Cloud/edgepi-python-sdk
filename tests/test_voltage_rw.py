55ae3a914c37fce204cf00a77d00bab2c1cea23e
flavius-t <flavius.t@hotmail.com>
2022-11-23 15:00:53 -0800

    raise assertion error

--- tests/test_voltage_rw.py
+++ tests/test_voltage_rw.py
@@ -41,24 +41,37 @@ def _measure_voltage(adc, dac, adc_num: ADCNum, dac_ch: int, write_voltage: floa
     # write to DAC channel
     dac.write_voltage(dac_ch, write_voltage)
 
+    num_failed = 0
     for _ in range(NUM_ADC_READS):
         read_voltage = adc.read_voltage(adc_num)
         try:
             assert write_voltage == pytest.approx(read_voltage, abs=RW_ERROR)
         except AssertionError as err:
-            _logger.error(f"voltage read error exceeds tolerance: {err}")
+            _logger.error(
+                    (
+                        "voltage read-write error exceeds tolerance: "
+                        f"write_voltage={write_voltage} V, read_voltage={read_voltage} V\n{err}"
+                    )
+                )
+            num_failed += 1
+
+    return num_failed
 
 def test_voltage_rw_adc_1(adc, dac):
     # set ADC read channel
     adc.start_conversions(ADCNum.ADC_1)
 
+    num_failed = 0
     for ch in range(NUM_CHANNELS):
         adc.set_config(adc_1_analog_in=_ch_map[ch][0])
 
         voltage = 0
         while voltage <= MAX_VOLTAGE:
-            _measure_voltage(adc, dac, ADCNum.ADC_1, _ch_map[ch][1], voltage)
+            print(ch, voltage)
+            num_failed += _measure_voltage(adc, dac, ADCNum.ADC_1, _ch_map[ch][1], voltage)
             voltage += VOLTAGE_STEP
-            
-    
+
     adc.stop_conversions(ADCNum.ADC_1)
+
+    if num_failed > 0:
+        raise AssertionError("voltage read-write test failed")
