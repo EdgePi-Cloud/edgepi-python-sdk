ae6148d556fdb88e207af54f06e9cea4aacd657c
flavius <flavius.t@hotmail.com>
2022-12-01 14:50:55 -0800

    fix rtd temp calculation bug

--- src/edgepi/adc/adc_voltage.py
+++ src/edgepi/adc/adc_voltage.py
@@ -104,7 +104,7 @@ def code_to_temperature(
     code_bits = bitstring_from_list(code)
 
     # refer to https://github.com/osensa/edgepi-python-sdk/issues/159 for computation details
-    r_rtd = code_bits.uint / (2 << 30) * ref_resistance
+    r_rtd = code_bits.uint / (2 ** 30) * ref_resistance
 
     temperature = (r_rtd - rtd_sensor_resistance) / rtd_sensor_resistance_variation
     _logger.debug(f"computed rtd temperature = {temperature}, from code = {code_bits.uint}")
