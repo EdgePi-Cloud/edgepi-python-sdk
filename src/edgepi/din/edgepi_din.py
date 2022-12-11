abf607a0f1e8a0be761f542bd72e3ee6f7ff6a1e
sjpark608 <spark@osensa.com>
2022-12-11 12:11:03 -0800

    added exception and testing

--- src/edgepi/din/edgepi_din.py
+++ src/edgepi/din/edgepi_din.py
@@ -1,7 +1,11 @@
 """Digital Input Module"""
 
 from edgepi.gpio.gpio_constants import GpioPins
+from edgepi.gpio.gpio_configs import DINPins
 from edgepi.gpio.edgepi_gpio import EdgePiGPIO
+
+class InvalidPinName(Exception):
+    """Raised invalid pin name"""
 
 class EdgePiDIN():
     """handling reading of digital inputs"""
@@ -17,5 +21,6 @@ class EdgePiDIN():
         Return:
             state (bool): corresponding pin state
         """
+        if pin_name is None or pin_name.value not in [pins.value for pins in DINPins]:
+            raise InvalidPinName(f'Invalid pin name passed: {pin_name}')
         return self.gpio.read_pin_state(pin_name.value)
-        
--- src/test_edgepi/unit_tests/test_din/test_edgepi_din.py
+++ src/test_edgepi/unit_tests/test_din/test_edgepi_din.py
@@ -7,35 +7,39 @@ import sys
 if sys.platform != 'linux':
     sys.modules['periphery'] = mock.MagicMock()
 
+from contextlib import nullcontext as does_not_raise
 import pytest
 from edgepi.gpio.gpio_constants import GpioPins
-from edgepi.din.edgepi_din import EdgePiDIN
+from edgepi.din.edgepi_din import EdgePiDIN, InvalidPinName
 
 @pytest.fixture(name="din")
 def fixture_test_dac(mocker):
     mocker.patch('edgepi.gpio.edgepi_gpio.EdgePiGPIO')
-    # mocker.patch('edgepi.din.EdgePiDIN.EdgePiGPIO.EdgePiGPIOChip')
     yield EdgePiDIN()
 
-@pytest.mark.parametrize("pin_name, mock_value, result",
-                        [(GpioPins.DIN1, True, True),
-                         (GpioPins.DIN2, True, True),
-                         (GpioPins.DIN3, True, True),
-                         (GpioPins.DIN4, True, True),
-                         (GpioPins.DIN5, True, True),
-                         (GpioPins.DIN6, True, True),
-                         (GpioPins.DIN7, True, True),
-                         (GpioPins.DIN8, True, True),
-                         (GpioPins.DIN1, False, False),
-                         (GpioPins.DIN2, False, False),
-                         (GpioPins.DIN3, False, False),
-                         (GpioPins.DIN4, False, False),
-                         (GpioPins.DIN5, False, False),
-                         (GpioPins.DIN6, False, False),
-                         (GpioPins.DIN7, False, False),
-                         (GpioPins.DIN8, False, False)])
-def test_edgepi_digital_input_state(mocker, pin_name, mock_value, result, din):
+@pytest.mark.parametrize("pin_name, mock_value, result, error",
+                        [(GpioPins.DIN1, True, True, does_not_raise()),
+                         (GpioPins.DIN2, True, True, does_not_raise()),
+                         (GpioPins.DIN3, True, True, does_not_raise()),
+                         (GpioPins.DIN4, True, True, does_not_raise()),
+                         (GpioPins.DIN5, True, True, does_not_raise()),
+                         (GpioPins.DIN6, True, True, does_not_raise()),
+                         (GpioPins.DIN7, True, True, does_not_raise()),
+                         (GpioPins.DIN8, True, True, does_not_raise()),
+                         (GpioPins.DIN1, False, False, does_not_raise()),
+                         (GpioPins.DIN2, False, False, does_not_raise()),
+                         (GpioPins.DIN3, False, False, does_not_raise()),
+                         (GpioPins.DIN4, False, False, does_not_raise()),
+                         (GpioPins.DIN5, False, False, does_not_raise()),
+                         (GpioPins.DIN6, False, False, does_not_raise()),
+                         (GpioPins.DIN7, False, False, does_not_raise()),
+                         (GpioPins.DIN8, False, False, does_not_raise()),
+                         (GpioPins.DOUT2, False, False, pytest.raises(InvalidPinName)),
+                         (None, False, False, pytest.raises(InvalidPinName))])
+def test_edgepi_digital_input_state(mocker, pin_name, mock_value, result, error, din):
     mocker.patch("edgepi.gpio.edgepi_gpio.EdgePiGPIOChip.read_gpio_pin_state",
                  return_value = mock_value)
-    assert din.digital_input_state(pin_name) == result
+    with error:
+        state = din.digital_input_state(pin_name)
+        assert state == result
     
