7c55db114610b172ab845526df8789937c678d0b
sjpark608 <spark@osensa.com>
2022-12-11 10:48:55 -0800

    Digital input reading #170

--- src/test_edgepi/unit_tests/test_din/test_edgepi_din.py
+++ src/test_edgepi/unit_tests/test_din/test_edgepi_din.py
@@ -9,5 +9,20 @@ if sys.platform != 'linux':
 
 import pytest
 from edgepi.gpio.gpio_constants import GpioPins
-from edgepi.gpio.edgepi_gpio import EdgePiGPIO
+from edgepi.din.edgepi_din import EdgePiDIN
 
+@pytest.fixture(name="din")
+def fixture_test_dac(mocker):
+    mocker.patch('edgepi.gpio.edgepi_gpio.EdgePiGPIO')
+    # mocker.patch('edgepi.din.EdgePiDIN.EdgePiGPIO.EdgePiGPIOChip')
+    yield EdgePiDIN()
+
+@pytest.mark.parametrize("pin_name, mock_value, result",
+                        [(GpioPins.DIN1, True, True),
+                         (GpioPins.DIN2, True, True),
+                         (GpioPins.DIN3, True, True)])
+def test_edgepi_digital_input_state(mocker, pin_name, mock_value, result, din):
+    mocker.patch("edgepi.gpio.edgepi_gpio.EdgePiGPIOChip.read_gpio_pin_state",
+                 return_value = mock_value)
+    assert din.digital_input_state(pin_name) == result
+    
