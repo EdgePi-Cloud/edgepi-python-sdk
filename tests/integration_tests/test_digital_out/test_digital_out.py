4ae1791c5989c559e2d723b0b263b6d0aca949d6
sjpark608 <spark@osensa.com>
2022-12-29 16:04:52 -0800

    creating integration test

---
+++ tests/integration_tests/test_digital_out/test_digital_out.py
@@ -0,0 +1,36 @@
+'''Integration tests for edgepi_digital_output.py module'''
+
+import pytest
+import time
+from edgepi.digital_output.edgepi_digital_output import EdgePiDigitalOutput
+from edgepi.gpio.gpio_constants import GpioPins
+
+@pytest.mark.parametrize("pin_name", [
+    (GpioPins.DOUT1),
+    (GpioPins.DOUT2),
+    (GpioPins.DOUT3),
+    (GpioPins.DOUT4),
+    (GpioPins.DOUT5),
+    (GpioPins.DOUT6),
+    (GpioPins.DOUT7),
+    (GpioPins.DOUT8),
+])
+def test_output_high(pin_name):
+    dout=EdgePiDigitalOutput()
+    dout.digital_output_direction(pin_name, True)
+    time.sleep(1)
+
+@pytest.mark.parametrize("pin_name", [
+    (GpioPins.DOUT1),
+    (GpioPins.DOUT2),
+    (GpioPins.DOUT3),
+    (GpioPins.DOUT4),
+    (GpioPins.DOUT5),
+    (GpioPins.DOUT6),
+    (GpioPins.DOUT7),
+    (GpioPins.DOUT8),
+])
+def test_output_low(pin_name):
+    dout=EdgePiDigitalOutput()
+    dout.digital_output_direction(pin_name, False)
+    time.sleep(1)
