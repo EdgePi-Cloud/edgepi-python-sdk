ba090154b2905fe2243756f273d1e7d5ce213e49
sjpark608 <78987042+sjpark608@users.noreply.github.com>
2023-01-06 09:54:00 -0800

    Dev digital io integration test (#230)
    * digital input integration test
    
    #229
    
    * hardware test added
    
    * Update test_digital_io.py
    
    * Update test_digital_io.py
    
    * Added hardware test and fixed pylint
    
    * fixing pylint

---
+++ tests/integration_tests/test_digital_in/test_digital_in.py
@@ -0,0 +1,20 @@
+'''Integration tests for edgepi_digital_input.py module'''
+
+import pytest
+from edgepi.digital_input.edgepi_digital_input import EdgePiDigitalInput
+from edgepi.gpio.gpio_constants import GpioPins
+
+@pytest.mark.parametrize("pin_name", [
+    (GpioPins.DIN1),
+    (GpioPins.DIN2),
+    (GpioPins.DIN3),
+    (GpioPins.DIN4),
+    (GpioPins.DIN5),
+    (GpioPins.DIN6),
+    (GpioPins.DIN7),
+    (GpioPins.DIN8),
+])
+def test_input_state(pin_name):
+    din=EdgePiDigitalInput()
+    pin_state = din.digital_input_state(pin_name)
+    assert pin_state is False
