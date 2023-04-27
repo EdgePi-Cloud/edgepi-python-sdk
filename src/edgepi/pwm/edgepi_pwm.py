8ed5b7fe3ec3957939815fa4e92f07e4a00683e0
sjpark608 <spark@osensa.com>
2023-04-27 15:06:03 -0700

    pwm module started

--- src/edgepi/pwm/edgepi_pwm.py
+++ src/edgepi/pwm/edgepi_pwm.py
@@ -15,13 +15,65 @@ class EdgePiPWM(PwmDevice):
                        polarity: Polarity = None):
         # Control internal mux to enable/disable PWM
         self.gpio = EdgePiGPIO()
-        self.chip = pwm_num.value.chip
-        self.channel = pwm_num.value.channel
+        super().__init__(channel=pwm_num.value.chip, chip=pwm_num.value.chip)
+        self.open_pwm()
         self.freq = freq
         self.duty_cycle = duty_cycle
         self.polarity = polarity.value
-    
 
 # TODO: set PWM, change GPIO pins
 # TODO: set freq, duty cycle
 # TODO: close PWM
+    def set_frequency(self, frequency: int):
+        """
+        Set frequency
+        """
+    def get_frequency(self):
+        """
+        Get frequency
+        """ 
+
+    def set_dutycycle(self, duty_cycle: float):
+        """
+        Set Duty Cycle
+        """
+    
+    def get_dutycycle(self):
+        """
+        get Duty cycle
+        """
+
+    def set_polarity(self, polarity: Polarity):
+        """
+        Set Duty Cycle
+        """
+    
+    def get_polarity(self):
+        """
+        get Duty cycle
+        """
+    
+    def enable_pwm(self):
+        """
+        Enable pwm output
+        """
+    
+    def disable_pwm(self):
+        """
+        Disable pwm output
+        """
+
+    def get_enabled(self):
+        """
+        Get enabled state
+        """
+
+    def close(self):
+        """
+        Close PWM connection
+        Args:
+            N/A
+        Returns:
+            N/A
+        """
+        self.close_pwm()
