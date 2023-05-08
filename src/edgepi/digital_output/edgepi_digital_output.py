53c7979301a3f5cb45d8c4ea25f253a1e9c02dc7
sjpark608 <spark@osensa.com>
2023-05-08 16:49:38 -0700

    Update edgepi_digital_output.py
    revised dout off logic

--- src/edgepi/digital_output/edgepi_digital_output.py
+++ src/edgepi/digital_output/edgepi_digital_output.py
@@ -1,6 +1,7 @@
 
 """Digital Output Module"""
 
+import time
 from edgepi.gpio.gpio_constants import GpioPins
 from edgepi.gpio.gpio_configs import DOUTPins
 from edgepi.gpio.edgepi_gpio import EdgePiGPIO
@@ -38,8 +39,10 @@ class EdgePiDigitalOutput():
         if state:
             self.gpio.set_pin_state(pin_name.value)
         else:
+            self.gpio.set_pin_state(self._dout_aout_pair[pin_name].value)
             self.gpio.clear_pin_state(pin_name.value)
-        self.gpio.clear_pin_state(self._dout_aout_pair[pin_name].value)
+            time.sleep(0.2)
+            self.gpio.clear_pin_state(self._dout_aout_pair[pin_name].value)
 
     def digital_output_direction(self, pin_name: GpioPins = None, direction: bool = None):
         """
