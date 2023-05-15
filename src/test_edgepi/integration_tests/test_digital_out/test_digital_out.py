97a41d748e54cac650c1cae5c464803e175698de
sjpark608 <78987042+sjpark608@users.noreply.github.com>
2023-05-15 15:43:28 -0700

    Dev dout get state (#312)
    * added get_state
    
    - added unit and integration tests
    
    * fixed pylint

--- src/test_edgepi/integration_tests/test_digital_out/test_digital_out.py
+++ src/test_edgepi/integration_tests/test_digital_out/test_digital_out.py
@@ -19,6 +19,8 @@ def test_output_high(pin_name):
     dout=EdgePiDigitalOutput()
     dout.digital_output_state(pin_name, True)
     time.sleep(1)
+    gpio_stat, _ = dout.get_state(pin_name)
+    assert gpio_stat is True
 
 @pytest.mark.parametrize("pin_name", [
     (GpioPins.DOUT1),
@@ -34,3 +36,39 @@ def test_output_low(pin_name):
     dout=EdgePiDigitalOutput()
     dout.digital_output_state(pin_name, False)
     time.sleep(1)
+    gpio_stat, _ = dout.get_state(pin_name)
+    assert gpio_stat is False
+
+@pytest.mark.parametrize("pin_name", [
+    (GpioPins.DOUT1),
+    (GpioPins.DOUT2),
+    (GpioPins.DOUT3),
+    (GpioPins.DOUT4),
+    (GpioPins.DOUT5),
+    (GpioPins.DOUT6),
+    (GpioPins.DOUT7),
+    (GpioPins.DOUT8),
+])
+def test_direction_in(pin_name):
+    dout=EdgePiDigitalOutput()
+    dout.digital_output_direction(pin_name, True)
+    time.sleep(1)
+    _, gpio_dir = dout.get_state(pin_name)
+    assert gpio_dir is True
+
+@pytest.mark.parametrize("pin_name", [
+    (GpioPins.DOUT1),
+    (GpioPins.DOUT2),
+    (GpioPins.DOUT3),
+    (GpioPins.DOUT4),
+    (GpioPins.DOUT5),
+    (GpioPins.DOUT6),
+    (GpioPins.DOUT7),
+    (GpioPins.DOUT8),
+])
+def test_direction_out(pin_name):
+    dout=EdgePiDigitalOutput()
+    dout.digital_output_direction(pin_name, False)
+    time.sleep(1)
+    _, gpio_dir = dout.get_state(pin_name)
+    assert gpio_dir is False
