9b288feb4d6f6f7a03f77823ee83362306549398
sjpark608 <spark@osensa.com>
2023-05-16 19:41:48 -0700

    Update test_edgepi_pwm.py

--- src/test_edgepi/integration_tests/test_pwm/test_edgepi_pwm.py
+++ src/test_edgepi/integration_tests/test_pwm/test_edgepi_pwm.py
@@ -16,7 +16,7 @@ def fixture_test_pwm():
 @pytest.fixture(name="pwm_dev_default")
 def fixture_test_pwm():
     pwm_dev_default = EdgePiPWM()
-    pwm_dev_default.init(PWMPins.PWM1)
+    pwm_dev_default.init_pwm(PWMPins.PWM1)
     pwm_dev_default.set_config(PWMPins.PWM1, 1000, 50, Polarity.NORMAL)
     yield pwm_dev_default
 
