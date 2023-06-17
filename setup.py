a68c7fedce6fa549402ff554aeb62b3404ac03c5
sjpark608 <spark@osensa.com>
2023-06-16 18:15:55 -0700

    modified sdk version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setuptools.setup(
     name="edgepi-python-sdk",
-    version="1.1.33",
+    version="1.1.32",
     author="S.Park",
     author_email="spark@osensa.com",
     description="EdgePi Python SDK package",
