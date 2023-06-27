680e1834019cdc6765e2e71d213504bf431bff4c
sjpark608 <spark@osensa.com>
2023-06-27 15:29:36 -0700

    adding test files

---
+++ src/test_edgepi/unit_tests/test_eeprom/test_edgepi_eeprom_data.py
@@ -0,0 +1,20 @@
+'''unit test for edgepi eeprom data'''
+# pylint: disable=C0413
+# pylint: disable=no-member
+# pylint: disable=no-name-in-module
+# https://github.com/protocolbuffers/protobuf/issues/10372
+from unittest import mock
+import os
+PATH = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.modules['periphery'] = mock.MagicMock()
+
+import pytest
+from edgepi.eeprom.edgepi_eeprom_data import EepromDataClass
+from edgepi.eeprom.proto_files import edgepi_module_pb2
+
+def read_binfile():
+    """Read the dummy serializedFile and return byte string"""
+    with open(PATH+"/edgepi_default_bin","rb") as fd:
+        b_string = fd.read()
+    return b_string
