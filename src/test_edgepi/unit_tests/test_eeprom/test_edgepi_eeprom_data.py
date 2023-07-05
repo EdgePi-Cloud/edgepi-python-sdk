76f22f1e22955637e0c794319842abdf2b2fef42
sjpark608 <78987042+sjpark608@users.noreply.github.com>
2023-07-05 11:14:57 -0700

    Dev eerpom dataclass refactor (#356)
    * new dataclass simplifying serializing and de-serializng process #350
    
    * adding test files
    
    * module not found error fixed
    
    * added edgepi_eepromg_dataclass
    
    * fixed linting
    
    * PR suggestion
    
    * removed unused attributes
    
    * modified adc module message
    
    * separtate folder for generated pb2 files and proto source files
    
    * dataclasses are moved to separate files
    
    * fixed import issue and pylint

---
+++ src/test_edgepi/unit_tests/test_eeprom/test_edgepi_eeprom_data.py
@@ -0,0 +1,45 @@
+'''unit test for edgepi eeprom data'''
+# pylint: disable=C0413
+# pylint: disable=no-member
+# pylint: disable=no-name-in-module
+# https://github.com/protocolbuffers/protobuf/issues/10372
+from unittest import mock
+import os
+PATH = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.modules['periphery'] = mock.MagicMock()
+
+from edgepi.eeprom.edgepi_eeprom_data import EepromDataClass
+from edgepi.eeprom.protobuf_assets.generated_pb2 import edgepi_module_pb2
+
+def read_binfile():
+    """Read the dummy serializedFile and return byte string"""
+    with open(PATH+"/edgepi_default_bin","rb") as fd:
+        b_string = fd.read()
+    return b_string
+
+def test_init_data_class():
+    edgepi_eeprom_data = EepromDataClass()
+    edgepi_eeprom_pb = edgepi_module_pb2.EepromData()
+    assert edgepi_eeprom_pb.ByteSize() == 0
+    for attr in edgepi_eeprom_data.__annotations__:
+        assert getattr(edgepi_eeprom_data, attr) is None
+
+
+def test_deserialize_pb():
+    default_bin = read_binfile()
+    edgepi_eeprom_pb = edgepi_module_pb2.EepromData()
+    edgepi_eeprom_pb.ParseFromString(default_bin)
+    edgepi_eeprom_data = EepromDataClass.extract_eeprom_data(edgepi_eeprom_pb)
+    for attr in edgepi_eeprom_data.__annotations__:
+        assert getattr(edgepi_eeprom_data, attr) is not None
+
+def test_serialize_pb():
+    default_bin = read_binfile()
+    edgepi_eeprom_pb = edgepi_module_pb2.EepromData()
+    edgepi_eeprom_pb_2 = edgepi_module_pb2.EepromData()
+    edgepi_eeprom_pb_2.ParseFromString(default_bin)
+    assert edgepi_eeprom_pb.ByteSize() == 0
+    edgepi_eeprom_data = EepromDataClass.extract_eeprom_data(edgepi_eeprom_pb_2)
+    edgepi_eeprom_data.populate_eeprom_module(edgepi_eeprom_pb)
+    assert edgepi_eeprom_pb.ByteSize() != 0
