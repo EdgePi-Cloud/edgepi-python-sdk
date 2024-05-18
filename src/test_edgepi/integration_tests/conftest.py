4fa0c4ccc775e3357a0ca6696225aae2e8b6af54
Gabe <24978329+EarthenSky@users.noreply.github.com>
2024-05-17 17:43:40 -0700

    add EEPROM fix

---
+++ src/test_edgepi/integration_tests/conftest.py
@@ -0,0 +1,24 @@
+import base64
+import hashlib
+import platform
+
+import pytest
+    
+from edgepi.eeprom.edgepi_eeprom import EdgePiEEPROM
+from edgepi.eeprom.eeprom_constants import DEFAULT_EEPROM_BIN_B64
+   
+@pytest.fixture(scope="session", autouse=True)
+def eeprom_reset(request):
+    edgepi_eeprom = EdgePiEEPROM()
+
+    if platform.node == "edgepi-intg2":
+        print('loading default eeprom image ...')
+        default_bin = base64.b64decode(DEFAULT_EEPROM_BIN_B64)
+        hash_res = hashlib.md5(default_bin)
+        print('reseting eeprom ...')
+        edgepi_eeprom.reset_edgepi_memory(hash_res.hexdigest(), default_bin)
+        print('done!')
+    else:
+        print("dont reset eeprom")
+ 
+    # request.addfinalizer()
